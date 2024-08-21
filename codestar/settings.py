5ecb1c00c1eeefc61b1d799c30a23d8b91d3ed16
barkode <konstantinks@gmail.com>
2024-08-21 10:52:26 +0100

    Add function that choses the correct value to debug variable from env

--- codestar/settings.py
+++ codestar/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 import dj_database_url
 
 if os.path.isfile("env.py"):
-    import env
+    import env  # noqa
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,7 +31,7 @@ SECRET_KEY = (
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True if os.environ.get("DEBUG") == "True" else False
 
 ALLOWED_HOSTS = [
     "8000-barkode-djangoblogci-3yvzt7vdbt0.ws.codeinstitute-ide.net",
