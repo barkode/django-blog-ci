d9230e288fdeddc0a036df0c082bcea1cf146f5f
barkode <konstantinks@gmail.com>
2024-08-21 15:02:31 +0100

    Add the commentt model

--- blog/models.py
+++ blog/models.py
@@ -17,3 +17,15 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     update_on = models.DateField(auto_now=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments"
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter"
+    )
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    create_on = models.DateField(auto_now_add=True)
