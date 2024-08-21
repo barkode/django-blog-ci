76d2d5223ef453f4d4d9bc556dc493f65ece6bc4
barkode <konstantinks@gmail.com>
2024-08-21 15:46:45 +0100

    Add meta class to Comment class

--- blog/models.py
+++ blog/models.py
@@ -18,6 +18,12 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     update_on = models.DateField(auto_now=True)
 
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
 
 class Comment(models.Model):
     post = models.ForeignKey(
@@ -28,4 +34,10 @@ class Comment(models.Model):
     )
     body = models.TextField()
     approved = models.BooleanField(default=False)
-    create_on = models.DateField(auto_now_add=True)
+    created_on = models.DateField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
