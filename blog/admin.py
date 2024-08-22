7c3e1ebd01372fa72eefad623a9fae862043ee1d
barkode <konstantinks@gmail.com>
2024-08-22 15:10:46 +0100

    Add additional search fields

--- blog/admin.py
+++ blog/admin.py
@@ -6,9 +6,9 @@ from .models import Comment, Post
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-    list_display = ("title", "slug", "status")
-    search_fields = ["title"]
-    list_filter = ("status",)
+    list_display = ("title", "slug", "status", "created_on")
+    search_fields = ["title", "content"]
+    list_filter = ("status", "created_on")
     prepopulated_fields = {"slug": ("title",)}
     summernote_fields = ("content",)
 
