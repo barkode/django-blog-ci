919275d7f849829190bfcf5ddc8f4ffb55f42eac
barkode <konstantinks@gmail.com>
2024-08-26 14:37:03 +0100

    enable serving of image files

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
 from django.db import models  # noqa
 
@@ -12,6 +13,7 @@ class Post(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
+    featured_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
