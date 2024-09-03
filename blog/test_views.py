daac680ebb9e8788f52654302a8e1e10bb80b042
barkode <konstantinks@gmail.com>
2024-09-03 23:21:08 +0100

    Testing views: GET set go!

--- blog/test_views.py
+++ blog/test_views.py
@@ -7,15 +7,18 @@ from .models import Post
 class TestBlogViews(TestCase):
 
     def setUp(self):
+
+        """Creates about me content"""
         self.user = User.objects.create_superuser(
             username = "maUsername",
             password = "myPassword",
             email = "test@test.com"
         )
-        self.post = Post(title = "Blog title", authot = self.user, slug = "blog-title", excerpt="Blog excerpt", content="Blog content", status=1)
+        self.post = Post(title = "Blog title", author = self.user, slug = "blog-title", excerpt="Blog excerpt", content="Blog content", status=1)
         self.post.save()
 
     def test_render_post_detail_page_with_comment_form(self):
+        """Verifies get request for about me containing a collaboration form"""
         response = self.client.get(reverse('post_detail', args=['blog-title']))
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"Blog title", response.content)
